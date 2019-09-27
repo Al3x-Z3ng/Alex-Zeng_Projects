3851b2da7a1868b195b90cdd36c5784bcd0c783e
Al3x-Z3ng <55299531+Al3x-Z3ng@users.noreply.github.com>
2019-09-27 15:00:26 -0700

    Assignment2

---
+++ Assignment2/src/Problem2.java
@@ -0,0 +1,14 @@
+public class Problem2
+{
+    public static void main(String[] args)
+    {
+        int room_number;
+        double number;
+        room_number = 113;
+        number = 2.71828;
+        String subject  = "Computer Science";
+        System.out.println( "This is room # " + room_number );
+        System.out.println( "e is close to " + number );
+        System.out.println( "I'm learning a bit about " + subject);
+    }
+}
---
+++ Assignment2/src/Schedule.java
@@ -0,0 +1,44 @@
+import javax.lang.model.element.NestingKind;
+
+public class Schedule
+{
+    public static void main(String[] args)
+    {
+        String c1 = "Chemistry 11";
+        String c2 = "Physics 11";
+        String c3 = "Senior Band";
+        String c4 = "Pre-Calculus 11";
+        String c5 = "Composition 11";
+        String c6 = "Mandarin 11";
+        String c7 = "Geography 12";
+        String c8 = "Computer Programming 11";
+        String t1 = "Ms.Liu";
+        String t2 = "Mr.Jung";
+        String t3 = "Mr.Robertson";
+        String t4 = "Ms.Ungurean";
+        String t5 = "Mr.Ross";
+        String t6 = "Ms.Zhang";
+        String t7 = "Ms.Clutchey";
+        String t8 = "Mr.Zaremba";
+
+        System.out.println("+-------------------------------------------------------------------+");
+        System.out.println("|1|                              " + c1 + "|                " + t1 + "|");
+        System.out.println("|2|                                " + c2 + "|               " + t2 + "|");
+        System.out.println("|3|                               " + c3 + "|          " + t3 + "|");
+        System.out.println("|4|                           " + c4 + "|           " + t4 + "|");
+        System.out.println("|5|                            " + c5 + "|               " + t5 + "|");
+        System.out.println("|6|                               " + c6 + "|              " + t6 + "|");
+        System.out.println("|7|                              " + c7 + "|           " + t7 + "|");
+        System.out.println("|8|                   " + c8 + "|            " + t8 + "|");
+        System.out.println("+-------------------------------------------------------------------+");
+
+
+
+
+
+
+
+
+    }
+
+}
