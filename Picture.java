73fd257f26f8fc2d6101cb11f97764e4d0e4e84b
Al3x-Z3ng <55299531+Al3x-Z3ng@users.noreply.github.com>
2019-10-03 13:58:12 -0700

    Game Picture

---
+++ Picture.java
@@ -0,0 +1,18 @@
+public class Picture
+{
+    public static void main(String[] args) {
+        System.out.println("         ------            ----------------- ");
+        System.out.println("        <| . .|>           |   Objective:  |   ");
+        System.out.println("        <|  - |>           |Kill the ENEMY!|");
+        System.out.println("         ------            ----------------- ");
+        System.out.println("           11    ");
+        System.out.println("      <---------->");
+        System.out.println("      <|   1HP  |>");
+        System.out.println("      <| 200pts |>");
+        System.out.println("      <---------->");
+        System.out.println("  -     11    11");
+        System.out.println("  -     11    11");
+        System.out.println("  -    OOOO  OOOO");
+
+    }
+}
